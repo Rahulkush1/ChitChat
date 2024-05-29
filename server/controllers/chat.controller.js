ea55ae62f1f3f15a4829e1d432469d7c8d35b59c
rails <rahul9713428971@gmail.com>
2024-05-29 15:20:41 +0530

    added addmembers chat api

--- server/controllers/chat.controller.js
+++ server/controllers/chat.controller.js
@@ -1,6 +1,7 @@
 import { ALERT, REFETCH_CHAT } from "../constants/events.js";
 import { getOtherMember } from "../lib/helper.js";
 import { Chat } from "../models/chat.model.js";
+import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -89,4 +90,58 @@ const getMyGroups = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, groups, "Group Fetched Successfully"));
 });
 
-export { newGroupChat, getMyChats, getMyGroups };
+const addMembers = asyncHandler(async (req, res) => {
+  const { chatId, members } = req.body;
+  if (!chatId) {
+    throw new ApiError(404, "Chat is not Found!");
+  }
+
+  if (!members || members.length < 1) {
+    throw new ApiError(400, "Please provide members");
+  }
+  const chat = await Chat.findById(chatId);
+
+  if (!chat.groupChat) {
+    throw new ApiError(400, "This is not a group chat!");
+  }
+
+  if (chat.creator.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not allowed to add members to this group");
+  }
+  const allNewMembersPromise = members.map((i) => User.findById(i, "name"));
+
+  const allNewMembers = await Promise.all(allNewMembersPromise);
+
+  const uniqueMembers = allNewMembers
+    .filter((i) => !chat.members.includes(i._id.toString()))
+    .map((i) => i._id);
+
+  const alreadyAddedMembers = allNewMembers.filter((i) =>
+    chat.members.includes(i._id)
+  );
+
+  chat.members.push(...uniqueMembers);
+
+  if (chat.members.length > 50) {
+    throw new ApiError(400, "Group members limit reached");
+  }
+
+  await chat.save();
+  const allUsersName = allNewMembers.map((i) => i.name).join(",");
+  emitEvent(
+    req,
+    ALERT,
+    chat.members,
+    `${allUsersName} has been added in the group`
+  );
+
+  emitEvent(req, REFETCH_CHAT, chat.members);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Members added successfully"));
+});
+
+
+
+export { newGroupChat, getMyChats, getMyGroups, addMembers };
