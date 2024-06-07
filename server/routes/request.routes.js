5f9344907e333633d09e1f821f00d46aac6179e3
rails <rahul9713428971@gmail.com>
2024-06-07 14:45:29 +0530

    added create request api

--- server/routes/request.routes.js
+++ server/routes/request.routes.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { createRequest } from "../controllers/request.controller.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
+
+router.route("/create").post(isAuthenticated, createRequest);
 
 export default router;
